bea2abacb9e197395eddc3c6cedc9c173ea65461
qtkite <jackywen4@gmail.com>
2020-08-28 21:41:00 +1000

    Genres + users util functions

--- src/include/genres.php
+++ src/include/genres.php
@@ -8,7 +8,7 @@
  * @author Jacky Wu
  */
 
- require_once("database.php");
+require_once("database.php");
 
 class Genres
 {
@@ -25,4 +25,28 @@ class Genres
         return $get_genres->fetchAll();
     }
 
+    /**
+     * Adds a new tag to the database
+     */
+    public function AddGenre($name)
+    {
+        $sql = $GLOBALS['db'];
+        $add_genre = $sql->prepare('INSERT INTO genres (tag, active) VALUES (:tag, :active)');
+        $add_genre->bindValue(':tag', $name);
+        $add_genre->bindValue(':active', 1);
+        $add_genre->execute();
+    }
+
+    /**
+     * Sets the state of a tag to be active or false
+     */
+    public function ToggleGenre($id, $state)
+    {
+        $sql = $GLOBALS['db'];
+        $set_genre = $sql->prepare('UPDATE genres SET active=:active WHERE id=:id');
+        $set_genre->bindValue(':active', $state ? 1 : 0);
+        $set_genre->bindValue(':id', $id);
+        $set_genre->execute();
+    }
+    
 }
--- src/include/users.php
+++ src/include/users.php
@@ -25,4 +25,63 @@ class Users
         return $get_users->fetchAll();
     }
 
+    /**
+     * Adds a new user to the database
+     * 
+     * @param password automatically handles bcrypt hashing at 10 rounds
+     */
+    public function AddUser($name, $password, $roles, $instrument, $gender, $bio, $pfp, $genre, $songs, $level)
+    {
+        $sql = $GLOBALS['db'];
+        $add_user = $sql->prepare('INSERT INTO users (name, password, roles, instrument, gender, bio, pfp, genre, songs, level, active) VALUES (:name, :password, :roles, :instrument, :gender, :bio, :pfp, :genre, :songs, :level, :active)');
+        $add_user->bindValue(':name', $name);
+        $add_user->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
+        $add_user->bindValue(':roles', $roles);
+        $add_user->bindValue(':instrument', $instrument);
+        $add_user->bindValue(':gender', $gender);
+        $add_user->bindValue(':bio', $bio);
+        $add_user->bindValue(':pfp', $pfp);
+        $add_user->bindValue(':genre', $genre);
+        $add_user->bindValue(':songs', $songs);
+        $add_user->bindValue(':level', $level);
+        $add_user->bindValue(':active', 1); // default active
+        $add_user->execute();
+    }
+
+    /**
+     * Toggles a user state given an ID
+     */
+    public function ToggleUser($id, $state)
+    {
+        $sql = $GLOBALS['db'];
+        $toggle_user = $sql->prepare('UPDATE users SET active=:active WHERE id=:id');
+        $toggle_user->bindValue(':active', $state ? 1 : 0);
+        $toggle_user->bindValue(':id', $id);
+        $toggle_user->execute();
+    }
+
+    /**
+     * Update the user's bio
+     */
+    public function EditBio($id, $bio)
+    {
+        $sql = $GLOBALS['db'];
+        $update_bio = $sql->prepare('UPDATE users SET bio=:bio WHERE id=:id');
+        $update_bio->bindValue(':bio', $bio);
+        $update_bio->bindValue(':id', $id);
+        $update_bio->execute();
+    }
+
+    /**
+     * Edits the user profile link in database
+     */
+    public function EditPfp($id, $pfp)
+    {
+        $sql = $GLOBALS['db'];
+        $update_pfp = $sql->prepare('UPDATE user SET pfp=:pfp WHERE id=:id');
+        $update_pfp->bindValue(':pfp', $pfp);
+        $update_pfp->bindValue(':id', $id);
+        $update_pfp->execute();
+    }
+
 }
